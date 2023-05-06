cebaf89e66333fffb7dc98bb9de9047b78545a68
Zii9 <132731744+Zii9@users.noreply.github.com>
2023-05-06 15:08:15 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "TestPlugin" 
+  
+ buildscript { 
+     repositories { 
+         google() 
+         mavenCentral() 
+         mavenLocal() 
+         maven("https://jitpack.io") 
+     } 
+  
+     dependencies { 
+         classpath("com.android.tools.build:gradle:7.0.4") 
+         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10") 
+         classpath("com.github.recloudstream:gradle:master-SNAPSHOT") 
+     } 
+ }
