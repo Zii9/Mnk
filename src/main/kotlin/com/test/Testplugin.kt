5382f20b9e8c37c269cedda9fae7cf37fe5b696c
Zii9 <132731744+Zii9@users.noreply.github.com>
2023-05-06 14:17:36 +0530

    Create Testplugin.kt

---
+++ src/main/kotlin/com/test/Testplugin.kt
@@ -0,0 +1,14 @@
+package com.test 
+  
+ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin 
+ import com.lagradost.cloudstream3.plugins.Plugin 
+ import com.lagradost.cloudstream3.APIHolder 
+ import android.content.Context 
+  
+ @CloudstreamPlugin 
+ class TestPlugin: Plugin() { 
+     override fun load(context: Context) { 
+         APIHolder.allProviders.add(TestProvider()) 
+         APIHolder.allProviders.add(xmoviesforyou()) 
+     } 
+ }
